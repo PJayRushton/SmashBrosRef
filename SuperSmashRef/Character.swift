fc517ef0fdeebba96668b70add777ae6215dfe64
Parker Rushton <parkerrushton@gmail.com>
2025-03-03 15:24:29 -0700

    Adds ai character data

--- SuperSmashRef/Character.swift
+++ SuperSmashRef/Character.swift
@@ -8,34 +8,31 @@
 import Foundation
 import SwiftUI
 
+struct Stats {
+    let power: Int // 1-7
+    let speed: Int // 1-7
+    let longRange: Int // 1-7
+    let recovery: Int // 1-7
+    let hasCounter: Bool
+}
+
 struct Character: Identifiable, Equatable {
     let id: String
     let name: String
     let order: Double
     let world: String
-
+    
     let mainImage: Image
     let thumbnailImage: Image
     let levelImage: Image
     let movesImage: Image
     let skinImages: [Image]
-
+    
     let isDLC: Bool
     let priceCents: Int?
-
-    init(
-        id: String,
-        name: String,
-        order: Double,
-        world: String,
-        mainImage: Image,
-        thumbnailImage: Image,
-        levelImage: Image,
-        skinImages: [Image] = [],
-        movesImage: Image,
-        isDLC: Bool = false,
-        priceCents: Int? = nil
-    ) {
+    let stats: Stats
+    
+    init(id: String, name: String, order: Double, world: String, mainImage: Image, thumbnailImage: Image, levelImage: Image, movesImage: Image, skinImages: [Image], stats: Stats, isDLC: Bool = false, priceCents: Int? = nil) {
         self.id = id
         self.name = name
         self.order = order
@@ -45,6 +42,7 @@ struct Character: Identifiable, Equatable {
         self.levelImage = levelImage
         self.movesImage = movesImage
         self.skinImages = skinImages
+        self.stats = stats
         self.isDLC = isDLC
         self.priceCents = priceCents
     }
